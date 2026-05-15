ddff6098e38cda2963f57600c228e39ea7ef158f
HunterIlya <137604900+HunterIlya@users.noreply.github.com>
2026-05-15 17:37:28 +0300

    .

---
+++ src/library/service/Storage.java
@@ -0,0 +1,29 @@
+package library.service;
+
+import library.model.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Storage {
+
+    public static List<User> users =
+            new ArrayList<>();
+
+    public static List<LibraryItem> items =
+            new ArrayList<>();
+
+    public static void showStorageState() {
+
+        System.out.println(
+                "Storage.showStorageState()"
+        );
+
+        System.out.println(
+                "Users count: " + users.size()
+        );
+
+        System.out.println(
+                "Items count: " + items.size()
+        );
+    }
+}
