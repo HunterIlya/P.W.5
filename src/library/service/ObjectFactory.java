20023fc477b12430bea1ab4d15008d02b0245843
HunterIlya <137604900+HunterIlya@users.noreply.github.com>
2026-05-15 17:37:04 +0300

    .

---
+++ src/library/service/ObjectFactory.java
@@ -0,0 +1,39 @@
+package library.service;
+
+import library.model.*;
+
+public class ObjectFactory {
+
+    public static User createUser(String name) {
+
+        System.out.println(
+                "ObjectFactory.createUser()"
+        );
+
+        return new User(name);
+    }
+
+    public static Book createBook(
+            String title,
+            Author author
+    ) {
+
+        System.out.println(
+                "ObjectFactory.createBook()"
+        );
+
+        return new Book(title, author);
+    }
+
+    public static ElectronicResource createElectronicResource(
+            String title,
+            String url
+    ) {
+
+        System.out.println(
+                "ObjectFactory.createElectronicResource()"
+        );
+
+        return new ElectronicResource(title, url);
+    }
+}
